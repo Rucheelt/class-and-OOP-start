c2e2b5c2cf7f791a693a01e2818ee2f214a6dff0
Rucheelt <narsingraosto@gmail.com>
2025-01-25 17:44:32 +0530

    modification in class

--- class 1.py
+++ class 1.py
@@ -1,5 +1,11 @@
 class school:
     sclname="JGI"
-    print(f"The name of the school is {sclname}")
+    name="kangaroo"
+    def stdintro(self):
+        print("Hi i am a student in this school studying in IB-2")
+        print(f"My name is {self.name}")
+        print(f"I study in {self.sclname}")
+
 
 scl=school()
+scl.stdintro()
